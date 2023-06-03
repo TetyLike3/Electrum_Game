e80fbef3b263feb9507efdebdd946ad64a40b43f
TetyLike3 <t.kerbalwhoflies@gmail.com>
2023-06-03 23:21:26 +0100

    Started writing code for Block class

---
+++ Block.cpp
@@ -0,0 +1,5 @@
+#include "Block.h"
+
+Block::Block(uint8_t size, std::string texturePath)
+{
+};
---
+++ Block.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include "VulkanEngine/Image.h"
+#include "VulkanEngine/Utilities.h"
+
+
+
+class Block
+{
+public:
+	Block(uint8_t size, std::string texturePath) : m_size(size), m_pUtilities(Utilities::getInstance()) {};
+
+private:
+	Utilities* m_pUtilities = nullptr;
+
+	uint8_t m_size = 1;
+	Image* m_pTextureImage = nullptr;
+};
